98b8c1e119b295bc38c586a9da8664102a5f72f4
jose mulatinho <j4mmbr@gmail.com>
2023-03-03 10:57:13 -0300

    using icomparable

---
+++ lambda-csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using lambda_csharp;
+
+List<Product> list = new List<Product>();
+
+list.Add(new Product("Cadeira", 200.00));
+list.Add(new Product("Armario", 700.00));
+list.Add(new Product("PC", 3000.00));
+
+list.Sort();
+
+foreach (Product p in list)
+{
+    Console.WriteLine(p);
+}
